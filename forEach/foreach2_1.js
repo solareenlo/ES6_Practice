25194ab6ace2098acaac15a37ff4e70666e8470f
solareenlo <solareenlo@gmail.com>
2018-10-19 02:17:42 +0900

    foreachで配列に入った数字の合計を求める

---
+++ forEach/foreach2_1.js
@@ -0,0 +1,13 @@
+// 数字の配列を用意する
+var numbers = [1, 2, 3, 4, 5];
+
+// 合計を保持する変数を用意する
+var sum = 0;
+
+// 配列の一つ一つの数字を合計に足す
+numbers.forEach(function(number) {
+  sum += number;
+});
+
+// 合計を表示する
+console.log(sum);
---
+++ forEach/foreach2_2.js
@@ -0,0 +1,14 @@
+// 数字の配列を用意する
+var numbers = [1, 2, 3, 4, 5];
+
+// 合計を保持する変数を用意する
+var sum = 0;
+
+function adder(number) {
+  sum += number;
+}
+// 配列の一つ一つの数字を合計に足す
+numbers.forEach(adder);
+
+// 合計を表示する
+console.log(sum);
