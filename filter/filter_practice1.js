1d80418804569d515fc0644d968b0860093b3d76
solareenlo <solareenlo@gmail.com>
2018-10-19 14:22:24 +0900

    filterを使って、配列中の50より大きな数を抜き出す

---
+++ filter/filter_practice1.js
@@ -0,0 +1,9 @@
+var numbers = [15, 25, 35, 45, 55, 65, 75, 85, 95];
+
+var filteredNumbers;
+
+filteredNumbers = numbers.filter(function(number) {
+   return number > 50;
+});
+
+console.log(filteredNumbers);
