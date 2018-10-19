8869f8bd580e45c503a32f968ea327e0c33f4cb9
solareenlo <solareenlo@gmail.com>
2018-10-19 17:40:40 +0900

    everyの使い所(入力フォーラムに全て文字が入っていたらサーバーにリクエストを投げるとか)

---
+++ every_some/every_some2.js
@@ -0,0 +1,29 @@
+function Field(value) {
+    this.value = value;
+}
+
+Field.prototype.validate = function() {
+    return this.value.length > 0;
+}
+
+var username = new Field('my_username');
+var password = new Field('my_password');
+var birthday = new Field('2010/10/10');
+
+var fields = [
+    username,
+    password,
+    birthday
+];
+
+var formIsValid = fields.every(function(field) {
+    return field.validate;
+});
+
+console.log(formIsValid);
+
+if(formIsValid) {
+    // サーバーにリクエストを投げる
+} else {
+    // エラーを表示する
+}
