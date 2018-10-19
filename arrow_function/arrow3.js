b0934c0daadd655028903e3fe97afc39f60cd58c
solareenlo <solareenlo@gmail.com>
2018-10-20 01:26:19 +0900

    arrow関数とthisとの関係性(arrow関数を使うとthisは、2個上のthisになる)

--- arrow_function/arrow3.js
+++ arrow_function/arrow3.js
@@ -6,7 +6,8 @@ const team = {
         return this.members.map((menber) => {
             // lexical this
             // コールバック関数の中のthisは別のところのthisだったけど、
-            // ES6からアロー関数を用いるとここのthisとして認識してくれる。
+            // ES6からアロー関数を用いると2個上のthisとして認識してくれる。
+            // teamSummaryが1個上で、const teamが2個上
             return `${menber}は${this.teamName}の所属です。`;
         });
     }
