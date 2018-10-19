41bfdc76739f66688f87e24c0d85f043a3b61414
solareenlo <solareenlo@gmail.com>
2018-10-19 13:59:10 +0900

    filterを使って複数の条件で絞り込みを行う

---
+++ filter/filte2.js
@@ -0,0 +1,15 @@
+var products = [
+    {name: 'きゅうり', type: '野菜', quantity: 0, price: 1},
+    {name: 'バナナ', type: 'フルーツ', quantity: 10, price: 15},
+    {name: 'きゅうり', type: '野菜', quantity: 30, price: 9},
+    {name: 'オレンジ', type: 'フルーツ', quantity: 3, price: 5},
+];
+
+var test = [];
+
+// 種類が野菜で、量が0個より多くて、値段が10より小さい
+test = products.filter(function(product){
+    return product.type==='野菜' && product.quantity>0 && product.price<10;
+});
+
+console.log(test);
