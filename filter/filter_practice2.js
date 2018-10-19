ead7d6730f102329e1a000fb6b586db6a88a83c9
solareenlo <solareenlo@gmail.com>
2018-10-19 14:27:45 +0900

    オブジェクト配列の中から特定の条件のものだけを抜き出す

---
+++ filter/filter_practice2.js
@@ -0,0 +1,15 @@
+var users = [
+ { id: 1, admin: true },
+ { id: 2, admin: false },
+ { id: 3, admin: false },
+ { id: 4, admin: false },
+ { id: 5, admin: true },
+];
+
+var filteredUsers;
+
+filteredUsers = users.filter(function(user) {
+    return user.admin === true;
+});
+
+console.log(filteredUsers);
