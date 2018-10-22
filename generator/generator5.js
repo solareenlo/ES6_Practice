b23457f8e8ba998930fc24b26f149fe58fe56323
solareenlo <solareenlo@gmail.com>
2018-10-22 22:53:37 +0900

    forofを使って、generatorの中身を配列にコピー

---
+++ generator/generator5.js
@@ -0,0 +1,13 @@
+function* colors() {
+    yield 'red';
+    yield 'blue';
+    yield 'green';
+}
+
+const myColors = [];
+
+for(let color of colors()) {
+    myColors.push(color);
+}
+
+console.log(myColors);
