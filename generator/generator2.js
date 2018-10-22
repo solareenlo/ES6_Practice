2453bf083d690d7072a111366afcbb7afe3d868f
solareenlo <solareenlo@gmail.com>
2018-10-22 14:29:23 +0900

    generatorのとりあえずの書き方その２

---
+++ generator/generator2.js
@@ -0,0 +1,14 @@
+function* shopping() {
+    // 歩道
+    // 歩道を歩いてお店に行く
+    // お店に到着したのでお金を持ってお店に入る
+    const stuffFromStore = yield 'お金';
+    // 家に歩いて帰る
+    return stuffFromStore;
+}
+
+// お店関連の世界
+const gen = shopping();
+console.log(gen.next()); // 家から歩道に出る
+console.log(gen.next('日用品')); // お店で買い物をして日用品を持って歩道に出る
+
